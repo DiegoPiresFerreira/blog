81db75a5dac442cfe8f5d3e94d156bcc6c74cc9f
Diego <diegopires0001@gmail.com>
2021-04-15 00:54:27 -0300

    cadastro de categorias no banco de dados

--- categories/categories_controller.js
+++ categories/categories_controller.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const category = require('./category');
+const slugify = require('slugify')
 
 
-router.get('/categories',(req,res) => {
-    res.send("rOTA DE CATEGORIA");
+router.get('/admin/categories/new',(req,res) => {
+    res.render('admin/categories/new');
 })
 
-router.get("/admin/categories/new",(req,res) => {
-
+router.post("/categories/save",(req,res) => {
+    const title = req.body.title
+    const slug = slugify(title)
+    if(title!=undefined){
+        category.create({
+            title,
+            slug
+        }).then(() => {
+            res.redirect('/');
+        })
+    }else{
+        res.redirect("/admin/categories/new");
+    }
 })
 
 
